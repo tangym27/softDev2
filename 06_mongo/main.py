84d1e931ecca1e8b8d567f126b02cfedc8ee5177
Michelle Tang <mtang1@stuy.edu>
2019-03-01 10:37:13 -0500

    fixed

--- 06_mongo/main.py
+++ 06_mongo/main.py
@@ -1,5 +1,10 @@
+#LetThatMongo -- Thomas Lee && Michelle Tang
+#SoftDev2 pd06
+#K06 -- Yummy Mongo Py
+#2019-02-28
+
 import pymongo
-SERVER_ADDR = " "
+SERVER_ADDR = "104.248.230.23"
 connection = pymongo.MongoClient(SERVER_ADDR)
 db = connection.test
 collection = db.restaurants
@@ -26,7 +31,14 @@ def query_zipcode_grade(zip, grade):
 
 # All restaurants in a specified zip code with a score below a specified threshold.
 def query_threshold(zip, score):
-    zip = str(zip)
+zip = str(zip)
 	restaurants = collection.find({'$and': [{"address.zipcode": zip},{"grades.0.score":{ '$lt': score }}]});
 	for restaurant in restaurants:
 		print(restaurant)
+
+# All restaurants in a specified zip code that is one of two specified cuisines
+def query_creative(zip, cuisA, cuisB):
+    zip = str(zip)
+    restaurants = collection.find({'$and': [{"address.zipcode": zip }, '$or': [{"cuisine": cuisA}, {"cuisine": cuisB }]]})
+    for restaurant in restaurants:
+        print(restaurant)
