ab2ca43e5f949f68141fff89582c071f6c8d2f1f
Michelle Tang <mtang1@stuy.edu>
2019-04-17 06:23:45 -0400

    listcomp

--- 19_listcomp/listcomp.py
+++ 19_listcomp/listcomp.py
@@ -1,29 +1,29 @@
-# Student Union (SU)
+# Student Union (SU) - Ricky Lin and Michelle Tang
+# SoftDev2 pd06
+# K#19: Ready, Set, Math!
+# 2019-04-17
 
+#union
 def union(a, b):
-    # a.extend(b)
-    # print a
     return [element for element in a if element not in b] + b
 
-    # return [a[element] for element in range(0, len(a)) for element2 in (element,len(a)-1) if not a[element] == a[element2]]
-# NOT WORKING YET
-print "union"
-print union([1,2,3], [2,3,4])
-
+print("union")
+print(union([1,2,3], [2,3,4]))
 
 #intersection
 def intersection(a, b):
     return [element for element in a for element2 in b if element == element2]
-print "intersection"
-print intersection([1,2,3], [2,3,4])
+
+print("intersection")
+print(intersection([1,2,3], [2,3,4])
 
 
 #set difference, find what is not in it
 def difference(a, b):
     return [element for element in a if element not in b]
 
-print "difference"
-print difference([1,2,3], [2,3,4])
+print("difference")
+print(difference([1,2,3], [2,3,4]))
 
 def symDiff(a, b):
     return union(difference(a, b), difference(b, a))
@@ -39,3 +39,6 @@ print(cartProd([1,2], ['red','white']))
 
 def even(a, b):
     return [element for element in union(a,b) if element % 2 == 0]
+
+print("even")
+print(even([1,2,3], [2,3,4]))
