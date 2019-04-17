fab79d2918981a39a61b1e64cd6a93dd5c3f6792
tangym27 <mtang1@stuy.edu>
2019-04-17 12:04:18 -0400

    almost done

--- 20_listcomp/listcomp.py
+++ 20_listcomp/listcomp.py
@@ -1,4 +1,8 @@
-# to the lighthouse
+# to the lighthouse: Clara Mohri & Michelle Tang
+# SoftDev pd6
+# K #20: wee
+# 2019-04-17
+
 from functools import reduce
 
 def open_file(file):
@@ -18,12 +22,13 @@ def single_freq(word, text):
     return reduce((lambda x,y: x+y), list)
 
 def mult_freq(words, text):
-    words = words.split().lower()
+    words = strip(words.split())
     list = [1 for x in text if x == word]
     if not len(list):
         return 0
     return reduce((lambda x,y: x+y), list)
-# "hi there"
+
+# ["hi","there"]
 text = open_file("hi.txt")
 text = strip(text)
 print(single_freq("there", text))
